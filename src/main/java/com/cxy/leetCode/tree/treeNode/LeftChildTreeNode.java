470416af2f6cea47a065e0bda310f6dcadb0752e
cxy <694975984@qq.com>
2020-05-09 18:05:16 +0800

    树的leftchild array表示法 和 child List表示

---
+++ src/main/java/com/cxy/leetCode/tree/treeNode/LeftChildTreeNode.java
@@ -0,0 +1,21 @@
+package com.cxy.leetCode.tree.treeNode;
+
+import java.util.LinkedList;
+
+/**
+ * Description: 左孩子兄弟链表表示法的 节点   <br>
+ * Date: 2020/5/9 14:38  <br>
+ *
+ * @author :cxy <br>
+ * @version : 1.0 <br>
+ */
+public class LeftChildTreeNode<T> {
+    //数据
+     T data;
+
+     //其他兄弟节点
+     LinkedList<LeftChildTreeNode> siblings;
+
+     //左孩子
+    LeftChildTreeNode<T> leftChild;
+}
