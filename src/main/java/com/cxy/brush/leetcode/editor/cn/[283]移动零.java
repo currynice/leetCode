79f019598aa0ce78000bd4658bd2f74bf1574b44
chengxinyu-surface <694975984@qq.com>
2020-07-15 00:35:23 +0800

    移动零优化，避免大量非0数的自身交换，减少操作次数

--- src/main/java/com/cxy/brush/leetcode/editor/cn/[283]移动零.java
+++ src/main/java/com/cxy/brush/leetcode/editor/cn/[283]移动零.java
@@ -26,13 +26,21 @@ class Solution283 {
         int j = 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i] != 0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
+                if(i!=j){
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
                 j++;
             }
         }
 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,0,3,12};
+       moveZeroes(nums);
+       System.out.println();
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
