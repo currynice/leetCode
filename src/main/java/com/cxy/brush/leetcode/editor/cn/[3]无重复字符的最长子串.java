73f3d79628265b145cb0c80d531e95ba2b1d73ab
cxy <694975984@qq.com>
2020-07-15 15:44:54 +0800

    todoList

--- src/main/java/com/cxy/brush/leetcode/editor/cn/[3]无重复字符的最长子串.java
+++ src/main/java/com/cxy/brush/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -1,4 +1,5 @@
-package com.cxy.brush.leetcode.editor.cn;//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+package com.cxy.brush.leetcode.editor.cn;
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
 //
 // 示例 1: 
 //
@@ -19,7 +20,7 @@ package com.cxy.brush.leetcode.editor.cn;//给定一个字符串，请你找出�
 // 输入: "pwwkew"
 //输出: 3
 //解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+//     请注意，你的答案必须是 子串 的长度。
 // 
 // Related Topics 哈希表 双指针 字符串 Sliding Window 
 // 👍 3957 👎 0
