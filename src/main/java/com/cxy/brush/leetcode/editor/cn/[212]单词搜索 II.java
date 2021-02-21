21f7f0d817f8444c2e0d798f15dcbab94387a4ca
chengxinyu-surface <694975984@qq.com>
2021-02-21 22:11:23 +0800

    0221

--- src/main/java/com/cxy/brush/leetcode/editor/cn/[212]单词搜索 II.java
+++ src/main/java/com/cxy/brush/leetcode/editor/cn/[212]单词搜索 II.java
@@ -1,6 +1,7 @@
 package com.cxy.brush.leetcode.editor.cn;//给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words，找出所有同时在二维网格和字典中出现的单词。
 //
-// 单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使
+// 单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+// 同一个单元格内的字母在一个单词中不允许被重复使
 //用。 
 //
 // 
@@ -41,7 +42,7 @@ package com.cxy.brush.leetcode.editor.cn;//给定一个 m x n 二维字符网格
 import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution212 {
 
     /***
      * 回溯算法
@@ -90,59 +91,76 @@ class Solution {
 //    }
 
     /***
-     * 字典树法(trie 需要带假删除功能，因为整个算法的时间复杂度取决于 Trie 的大小。对于 Trie 中的叶节点，一旦遍历它（即找到匹配的单词），就不需要再遍历它了。结果，我们可以把它从树上剪下来)
+     * 字典树法(trie 需要带删除功能，因为整个算法的时间复杂度取决于 Trie 的大小。对于 Trie 中的叶节点，一旦遍历它（即找到匹配的单词），就不需要再遍历它了。结果，我们可以把它从树上剪下来)
      * @param board
      * @param words
      * @return
      */
     public List<String> findWords(char[][] board, String[] words) {
-        Set<String> result = new HashSet<>();
-        //使用候选词生成一个字典树
-        TrieNode wordsTrie = new TrieNode(' ');
+        //直接使用ArrayList保存result，以及将扫到的trieNode.word!=null的节点设为Null,
+        // 避免重复添加进result
+        List<String> result = new ArrayList<>();
+        //候选词array -> 一个字典树
+        TrieNode wordsTrie = new TrieNode(' ');//root节点是空字符
         for(String word:words){
-            wordsTrie.insert(word);
+            TrieNode cur = wordsTrie;//root节点是空字符
+            for(char c:word.toCharArray()){
+                if(cur.children[c-'a']==null){
+                    //当前节点还没值，赋值
+                    cur.children[c-'a']=new TrieNode(c);
+                }
+                //临时更新
+                cur = cur.children[c-'a'];
+            }
+            //一个单词结束,立flag
+            cur.word = word;
         }
 
-        //遍历二维网格
+       //使用boolean[][] 保存已访问格
         boolean[][] visuted = new boolean[board.length][board[0].length];
+        //遍历二维网格
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
-                helper2(board,i,j,"",wordsTrie,result,visuted);
+                //当前格子字符直接不是树的一层节点,pass
+                if(wordsTrie.children[board[i][j]-'a'] != null){
+                    helper2(board,i,j,wordsTrie,result,visuted);
+                }
             }
         }
-        return new ArrayList<>(result);
+        return result;
     }
 
     //board[i][j] 是否是字典树trie的节点, 且board[i][j] 的左，下，右，上 是否存在任意一个是字典树trie.getSubNode(board[i][j])的节点
-    private void helper2(char[][] board, int i, int j,String str, Trie trie,Set<String> result,boolean[][] visuted){
-        //暂存中心点
-        // i,j非法,越界 或 board[i][j]已经被访问了,结束
+    private void helper2(char[][] board, int i, int j,TrieNode cur,List<String> result,boolean[][] visuted){
+
+        // i,j非法,越界 或 board[i][j]在探寻当前单词时已经被访问了,结束
         if(i<0||i>=board.length||j<0||j>=board[0].length||visuted[i][j]){
             return;
         }
-        str+=board[i][j];
+        //暂存中心点
+        char c = board[i][j];
 
-        //str是否是trie的前缀
-        if(!trie.startsWith(str)){
-            //当前str不是trie的前缀，不是一个单词
+        if(cur.children[c-'a']==null){
+            //无路可走，或这条成功的路已经被走过了
             return;
         }
-        //str完整的出现在trie中，意味着找到
-        if(trie.search(str)){
-            //board[i][j] 是字典树的叶子节点，结束
-            result.add(str);
-            //特殊处理:在树中删除该单词最后一个字母对应的叶子节点
 
+        cur = cur.children[c-'a'];//即将开始找下一行节点
+        if(cur.word!=null){
+           //叶子节点
+            result.add(cur.word);
+            //防止重复添加结果
+            cur.word = null;
         }
 
         //将中心点标记为已访问
         visuted[i][j] =true;
 
         //开始左，下，右，上 DFS探索 word.charAt(index+1)
-        helper2(board,i-1,j,str,trie,result,visuted);
-        helper2(board,i,j+1,str,trie,result,visuted);
-        helper2(board,i+1,j,str,trie,result,visuted);
-        helper2(board,i,j-1,str,trie,result,visuted);
+        helper2(board,i-1,j,cur,result,visuted);
+        helper2(board,i,j+1,cur,result,visuted);
+        helper2(board,i+1,j,cur,result,visuted);
+        helper2(board,i,j-1,cur,result,visuted);
 
         //不管找没找到，还原board[i][j]，因为后续该点还会使用到
         visuted[i][j] =false;
@@ -154,7 +172,7 @@ class Solution {
         private char val;
 
        private TrieNode[] children;
-       //用string!=null 代替boolean表示是否是一个word，更方便
+       //用string!=null 代替isEnd = true 表示该节点代表一个合法的word，更方便
        private String word;
 
        public TrieNode(char x) {
---
+++ src/main/java/com/cxy/brush/leetcode/editor/cn/[231]2的幂.java
@@ -0,0 +1,36 @@
+//给定一个整数，编写一个函数来判断它是否是 2 的幂次方。 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: true
+//解释: 20 = 1 
+//
+// 示例 2: 
+//
+// 输入: 16
+//输出: true
+//解释: 24 = 16 
+//
+// 示例 3: 
+//
+// 输入: 218
+//输出: false 
+// Related Topics 位运算 数学 
+// 👍 273 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * 2的幂次方，二进制位仅有一个1，且不是末位  0....1..0
+     * @param n
+     * @return
+     */
+    public boolean isPowerOfTwo(int n) {
+        long x = n;
+          return x!=0 && ((x&(x-1))==0);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
